2fb39407575d7d751c3c1ad8456d6d6dc6ff6d33
Mattéo <60100878+Matteo-Ynov@users.noreply.github.com>
2021-04-01 21:46:01 +0200

    Added calendar shit

---
+++ BridgeMonitor/Controllers/CalendarController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using BridgeMonitor.Models;
+using System.Linq;
+
+namespace BridgeMonitor.Controllers
+{
+    public class CalendarController : Controller
+    {
+
+        public static List<Boats> BoatsList = new List<Boats>();
+        public CalendarController()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php").Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        var json = content.ReadAsStringAsync().Result;
+                        BoatsList = JsonConvert.DeserializeObject<List<Boats>>(json);
+                        BoatsList = (from e in BoatsList
+                                        orderby e.ClosingDate
+                                        select e).ToList();
+                    }
+                }
+            }
+        }
+            // GET: CalendarController
+            public ActionResult Calendar()
+        {
+            return View(BoatsList);
+        }
+
+
+        // POST: CalendarController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CalendarController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CalendarController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CalendarController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CalendarController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
